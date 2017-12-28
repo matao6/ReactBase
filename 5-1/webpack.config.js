9bad1679592d659ce121ea57ec1d2c1b8f8000c6
matao6 <1152843670@qq.com>
2017-12-28 17:41:12 +0800

    first commit

---
+++ 5-1/webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    context: __dirname + '/src',
+    entry: './js/index.js',
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    output: {
+        path: __dirname + '/src/',
+        filename: 'bundle.js'
+    }
+}
